2899b21a43c03181de0cd5b64706e738292eb633
godfreyJo <godfreyoyugi@gmail.com>
2021-04-02 04:50:44 +0300

    Hellow world

---
+++ react/ttt/src/components/Gody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// this is a simple example of a functional component
+// function Gody() {
+//     return <h1>Vipi Mzima!!</h1>
+
+// }
+
+const Gody = () => <h1>Vipi Gody!!</h1>
+
+
+export default Gody
---
+++ react/ttt/src/components/Karibu.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+
+class Karibu extends Component {
+    render() {
+        return <h1>Mfano wa Class Component</h1>
+    }
+}
+
+export default Karibu
