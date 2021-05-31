3396b66d591e25a4dcd997ebe222492da31310f4
godfreyJo <godfreyoyugi@gmail.com>
2021-05-31 11:43:04 +0300

    executing the create book logic

--- books/Books/home/views.py
+++ books/Books/home/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, User, Borrower
+from .forms import BookForm
 
 # Create your views here.
 
+
+def home(request):
+    return render(request, 'home.html')
 
 def allbooks(request):
     books = Book.objects.all()
@@ -10,15 +14,49 @@ def allbooks(request):
     context = {  
     "books":books
     }
-    return render(request, 'home.html', context)
+    return render(request, 'books.html', context)
 
-def detailBook(request, pk):
+def book_details(request, pk):
     book = Book.objects.get(id=pk)
     context = {
     "book":book
     }
     
     return render(request, 'bookDetailed.html', context)
+
+def create_book(request):
+    form = BookForm()
+    if request.method == 'POST':
+        print('receiving a post request') 
+        form = BookForm(request.POST)
+        if form.is_valid():
+           
+            Book_Title = form.cleaned_data['Book_Title']
+            ISBN_Number = form.cleaned_data['ISBN_Number']
+            DatePublished = form.cleaned_data['DatePublished']
+            category = form.cleaned_data['category']
+            author = form.cleaned_data['author']
+            status = form.cleaned_data['status']
+            
+            Book.objects.create(
+                Book_Title=Book_Title,
+                ISBN_Number=ISBN_Number,
+                DatePublished=DatePublished,
+                category=category,
+                author=author,
+                status=status)
+           
+            return redirect('/books')
+
+                  
+       
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'bookCreate.html', context)
+
 
  
 
